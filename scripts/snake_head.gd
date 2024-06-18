518613ce98a89d218cdd058b3100f77f0fd9a477
Justin Lafferty <justin.lafferty@kw.com>
2024-06-18 11:34:16 -0400

    move the snake

--- scripts/snake_head.gd
+++ scripts/snake_head.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var snake_body_scene: PackedScene;
 
 var body_segments = [];
+var x_direction = 0;
+var y_direction = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,8 +14,12 @@ func _ready():
 	
 	for i in 2:
 		var body_segment = snake_body_scene.instantiate();
-		# The position is relative to the parent not the entire room
-		body_segment.position.y = i * 20;
+		
+		body_segment.position.x = position.x;
+		body_segment.position.y = position.y + (i * 20) + 20;
+		
+		body_segment.top_level = true;
+
 		add_child(body_segment);
 		body_segments.append(body_segment);
 
@@ -21,4 +27,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("move_right"):
+		x_direction = 20;
+		y_direction = 0;
+
+	if Input.is_action_just_pressed("move_left"):
+		x_direction = -20;
+		y_direction = 0;
+		
+	if Input.is_action_just_pressed("move_up"):
+		x_direction = 0;
+		y_direction = -20;
+		
+	if Input.is_action_just_pressed("move_down"):
+		x_direction = 0;
+		y_direction = 20;
+	
+	if (x_direction != 0 || y_direction != 0):
+		var tail = body_segments.pop_back();
+		tail.position.x = position.x;
+		tail.position.y = position.y;
+		
+		body_segments.push_front(tail);
+
+	position.x += x_direction;
+	position.y += y_direction;
